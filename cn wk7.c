f6f0046712d7559012867790c739ce18f6d7dafa
priaanka01 <apriaanka@gmail.com>
2025-05-04 22:23:26 +0530

    Update and rename cn week7.c to cn wk7.c

---
+++ cn wk7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int i, sn;
+    int st[20];
+    char op;
+    printf("How many stations: ");
+    scanf("%d", &sn);
+    do {
+        printf("\nEnter status of stations:\n");
+        for (i = 0; i < sn; i++) {
+            printf("Enter status of station %d (0 or 1): ", i + 1);
+            scanf("%d", &st[i]);
+        }
+        printf("\nStations ready to transmit:");
+        for (i = 0; i < sn; i++) {
+            if (st[i] == 1) {
+                printf("\nStation %d is ready to transmit", i + 1);
+            }
+        }
+        printf("\n\nRepeat? Press Y/y to continue: ");
+        scanf(" %c", &op);
+    } while (op == 'y' || op == 'Y');
+    return 0;
+}
