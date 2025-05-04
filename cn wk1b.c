b0d9c2c80c11e09cb4c09477e8214e2f2e99eb67
priaanka01 <apriaanka@gmail.com>
2025-05-04 13:13:02 +0530

    cn wk1b.c

--- cn wk1b.c
+++ cn wk1b.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
-   char a[100] ,b[100];
-   int i=0,j=0;
-   printf("Enter the string:");
-   scanf("%s",&a);
-   for(i=0;a[i]!='\0';i++)
-   {
-       if(a[i]=='D' && a[i+1]=='L'&& a[i+2]=='E'){
-           b[j++]='D';
-            b[j++]='L';
-              b[j++]='E';
-                b[j++]='D';
-                i+=2;
-       }
-       else{
-             b[j++]=a[i];
 
-       }
-   }
-   b[j]='\0';
-   printf("DLESTX%s DLEETX\n",b);
-   return 0;
+int main() {
+    char b[100];
+    int i = 0;
+
+
+    printf("\nEnter the string:\n");
+    fgets(b, sizeof(b), stdin);
+
+
+    size_t len = strlen(b);
+    if (b[len - 1] == '\n') {
+        b[len - 1] = '\0';
+    }
+
+
+    printf("\nAfter stuffing:\n");
+    printf("DLESTX");
+
+
+    while (i < strlen(b)) {
+
+        if ((b[i] == 'd' || b[i] == 'D') &&
+            (b[i+1] == 'i' || b[i+1] == 'I') &&
+            (b[i+2] == 'e' || b[i+2] == 'E')) {
+
+            printf("DLE");
+            i += 3;
+        } else {
+            printf("%c", b[i]);
+            i++;
+        }
+    }
+
+    printf("DLEETX\n");
 }
