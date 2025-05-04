ed55dbd898c4b5d8d1face86d594f5e49a72279e
priaanka01 <apriaanka@gmail.com>
2025-05-04 13:04:39 +0530

    cn wk1a.c

--- cn wk1a.c
+++ cn wk1a.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
-void main(){
-int a[50];
-int i,j,n,count=0,pos;
-printf("\n Enter the number of bits: ");
-scanf("%d",&n);
-printf("\n Enter the bits (0s and 1s):\n ");
-for(i=0; i<n;i++){
-    scanf("%d",&a[i]);
-}
-for(i=0;i<n;i++){
-    if(a[i]==1){
-    count++;
-    if(count==5){
-    pos=i+1;
-    for(j=n;j>pos;j--){
-    a[j]=a[j-1];
+
+void bitstuffing(int N, int arr[]) {
+    int count = 0;
+    int i, j = 0;
+
+    int stuffedArr[2 * N];
+
+    for (i = 0; i < N; i++) {
+        stuffedArr[j++] = arr[i];
+        if (arr[i] == 1) {
+            count++;
+            if (count == 5) {
+                stuffedArr[j++] = 0;
+                count = 0;
+            }
+        } else {
+            count = 0;
+        }
     }
-    a[pos]=0;
-    n++;
-    count=0;
-    i++;
+
+    for (i = 0; i < j; i++) {
+        printf("%d", stuffedArr[i]);
     }
+    printf("\n");
+}
+
+int main() {
+    int N = 6;
+    int arr[] = {1,1,1,1,1,1};
+
+    printf("Input binary array: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d", arr[i]);
     }
-    else{
-    count=0;
-    }
+    printf("\n");
+
+    printf("After bit stuffing: ");
+    bitstuffing(N, arr);
+
+    return 0;
 }
-printf("\n Data after stuffing:\n");
-for(i=0;i<n;i++){
-    printf("%d" ,a[i]);
-}
-printf("\n");
-}
+
