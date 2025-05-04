d65b5b8c7ddd7d0e3c0ddfb49a006d66c4b1ad7c
priaanka01 <apriaanka@gmail.com>
2025-05-05 01:13:15 +0530

    Update cn wk6.c

--- cn wk6.c
+++ cn wk6.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 int a[10][10], n;
 void adj(int k) {
-    int j;
-    printf("Adjacent nodes of root node %d::\n", k);
+    int i,j;
+    printf("Adjacent nodes of root node %d:\n", k);
     for (j = 1; j <= n; j++) {
         if (a[k][j] == 1 || a[j][k] == 1) {
             printf("%d\t", j);
+        }
+    }
+    printf("\n");
+    printf("Non-Adjacent nodes of root node %d:\n", k);
+    for(i=1;i<=n;i++){
+        if(i!=k && a[k][i]==0 && a[i][k]==0){
+            printf("%d\t", i);
         }
     }
     printf("\n");
